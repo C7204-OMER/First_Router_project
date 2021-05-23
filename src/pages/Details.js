ec2b2e3d89747aad6579481773db5ef4048ba863
7204-OMER <Eyupbhr24@gmail.com>
2021-05-23 15:54:23 +0300

    useParams 2

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react";
+import { useParams } from "react-router-dom";
 
 const Details = (props) => {
   const [post, setPost] = useState();
